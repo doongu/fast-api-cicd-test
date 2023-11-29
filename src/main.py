e2066a34a3b1dcf7ecd08a7f94ef543ec1d4c1e0
doongu <must1080@naver.com>
2023-11-29 18:00:11 +0900

    status_code 처리 완료

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from pydantic import BaseModel
 app = FastAPI()
 
@@ -33,9 +33,12 @@ def get_todos_handler(order: str | None = None):
         return ret[::-1]
     return ret
 
-@app.get("/todos/{todo_id}")
+@app.get("/todos/{todo_id}", status_code=200)
 def get_todo_handler(todo_id: int):
-    return todo_data.get(todo_id, {})
+    todo = todo_data.get(todo_id)
+    if todo:
+        return todo
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
 
 class CreateToDoRequest(BaseModel):
     id: int
@@ -44,13 +47,13 @@ class CreateToDoRequest(BaseModel):
 
 
 
-@app.post("/todos")
+@app.post("/todos", status_code=201)
 def create_todo_handler(request: CreateToDoRequest):
     todo_data[request.id] = request.dict()
     return todo_data[request.id]
 
 
-@app.patch("/todos/{todo_id}")
+@app.patch("/todos/{todo_id}", status_code=200)
 def update_todo_handler(
         todo_id: int,
         is_done: bool = Body(..., embed=True),
@@ -59,9 +62,11 @@ def update_todo_handler(
     if todo:
         todo["is_done"] = is_done
         return todo
-    return {}
+    raise HTTPException(status_code=400, detail="Not Found")
 
-@app.delete("/todos/todo_id")
+@app.delete("/todos/todo_id", status_code=204)
 def delete_todo_handler(todo_id: int):
-    todo_data.pop(todo_id, None)
-    return todo_data
+    todo = todo_data.pop(todo_id, None)
+    if todo:
+        return
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
