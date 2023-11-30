b8514b7755673001515b21c2dc1da4ee6747f70c
doongu <must1080@naver.com>
2023-11-30 17:54:51 +0900

    feat : session 관리를 위한 제너레이터 추가

--- src/database/connection.py
+++ src/database/connection.py
@@ -8,5 +8,10 @@ DATABASE_URL = "mysql+pymysql://root:todos@127.0.0.1:3306/todos"
 engine = create_engine(DATABASE_URL, echo=True)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-session = SessionFactory()
-session.query()
+
+def get_db():
+    session = SessionFactory()
+    try:
+        yield session
+    finally:
+        session.close()
